0ebd9a40a02749cde0c1913bd5f41bdfe4866997
Kyle Lauffer <kcl5072@gmail.com>
2023-12-23 09:38:03 -0500

    small update to cli for testing

--- kata-cli/main.ts
+++ kata-cli/main.ts
@@ -1,5 +1,14 @@
-  function main(): void {
-    console.log('Hello World');
-  }
-  
-  main();
+  import Denomander from "https://deno.land/x/denomander@0.9.3/mod.ts";
+
+  const program = new Denomander({
+    app_name: "Kata CLI",
+    app_description: "Clean Code Community Of Practice Command Line Interface",
+    app_version: "1.0.1",
+  });
+
+  program
+  .command("begin", "Begin a new kata practice session")
+  .action(() => {
+    console.log("Begin a new kata practice session");
+  })
+  .parse(Deno.args);
