c25edc5b7ff1a5d3f5a91efd86b0a17535e8e729
Kyle Lauffer <kcl5072@gmail.com>
2023-12-23 10:41:12 -0500

    testing update to install script

--- kata-cli/install.ts
+++ kata-cli/install.ts
@@ -16,6 +16,25 @@ async function install(): Promise<void> {
       console.error(`Error adding alias to .bashrc: ${error}`);
     }
   }
+
+  async function upgrade(): Promise<void> {
+    const aliasName = 'kata-cli-update';
+    const aliasCommand = 'deno cache --reload https://raw.githubusercontent.com/rsm-hcd/cop-clean-code-katas/main/kata-cli/main.ts';
+  
+    const bashrcPath = `${Deno.env.get('HOME')}/.bashrc`;
+  
+    const aliasLine = `\nalias ${aliasName}='${aliasCommand}'\n`;
+  
+    try {
+      await Deno.writeTextFile(bashrcPath, aliasLine, { append: true });
+      console.log(`Alias '${aliasName}' has been added to your .bashrc file.`);
+      console.log(`Your .bashrc file is located at '${bashrcPath}'.`);
+      console.log(`Restart your terminal or run 'source ${bashrcPath}' to apply changes.`);
+    } catch (error) {
+      console.error(`Error adding alias to .bashrc: ${error}`);
+    }
+  }
   
   await install();
+  await upgrade();
   
