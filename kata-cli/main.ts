4808bd8e33dd876a21e33f2f23eac97da7aeddce
Kyle Lauffer <kcl5072@gmail.com>
2024-01-02 10:35:00 -0500

    updated how cli command works when providing the name

--- kata-cli/main.ts
+++ kata-cli/main.ts
@@ -4,20 +4,20 @@ import { begin } from "./begin.ts";
 const program = new Denomander({
   app_name: "Kata CLI",
   app_description: "Clean Code Community Of Practice Command Line Interface",
-  app_version: "1.0.1",
+  app_version: "0.0.1",
 });
 
 program
   .command(
-    "begin",
+    "begin [name]",
     "Begin a new kata practice session by creating a new directory and the necessary files"
   )
-  .requiredOption("-n, --name", "The name of the kata to practice")
+  .argDescription("name", "The name of the kata to practice")
   .option(
     "-d, --directory",
     "The directory to create the kata in",
     undefined,
-    "."
+    "./"
   )
   .action(async () => {
     console.log(
