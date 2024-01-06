58d7ce2f09e34ae46456a64b114c5e6e7775269e
Kyle Lauffer <kcl5072@gmail.com>
2024-01-06 18:12:41 -0500

    removed environment variable

--- kata-cli/list-katas/list-kata-names/list-kata-names.ts
+++ kata-cli/list-katas/list-kata-names/list-kata-names.ts
@@ -1,11 +1,7 @@
 import { getDirectories } from "../list-directories/list-directories.ts";
 
 export async function listKataNames() {
-  const pathToKatas = Deno.env.get("PATH_TO_KATAS");
-
-  if (!pathToKatas) {
-    throw new Error("PATH_TO_KATAS environment variable not set.");
-  }
+  const pathToKatas = "../kata-templates";
   const kataNames = await getDirectories(pathToKatas);
   return kataNames;
 }
